2aabd9f1c439789e078e06d6d0abd32105a9a837
cann1neof <cann1neof@gmail.com>
2025-03-19 17:59:25 +0200

    task2 finished i guess

--- maman11/task2/count_zero_bits.c
+++ maman11/task2/count_zero_bits.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-int count_zero_bits();
-
-int main() {
-    printf("Hello World");
-    return 0;
+int count_zero_bits(int num){
+    unsigned int counter;
+    counter = 0;
+    while(num != 0){
+        if(num % 2 == 0){
+            counter++;
+        }
+        num /= 2;
+    }
+    return counter;
 }
 
+int main() {
+    unsigned int myNum;
+    printf("enter your fucking number\n");
+    scanf("%d", &myNum);
+    printf("you have %d zero bits in your fucking number\n", count_zero_bits(myNum));
+    return 0;
+}
