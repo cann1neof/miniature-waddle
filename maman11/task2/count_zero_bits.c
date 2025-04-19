286e96ae1e0b9b23159cbcd5010c70896fab9ea4
cann1neof <cann1neof@gmail.com>
2025-04-19 14:16:26 +0300

    maman11 task2 in progress

--- maman11/task2/count_zero_bits.c
+++ maman11/task2/count_zero_bits.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int count_zero_bits(int num){
-    unsigned int counter;
+unsigned int count_zero_bits(unsigned int num){
+    unsigned int counter = 0;
+    unsigned int length = 0;
+    unsigned int num2 = num;
+    // count leading zeros
+    while(num2 != 0){
+        num2 /= 2;
+        counter++;
+    }
+    
+    length = sizeof(num2) * 8 - length; // calculate number of leading zeros
     counter = 0;
+    // count trailing zeros
     while(num != 0){
         if(num % 2 == 0){
             counter++;
         }
         num /= 2;
     }
-    return counter;
+    return counter + length; // return total number of zeros
 }
 
 int main() {
     unsigned int myNum;
-    printf("enter your fucking number\n");
+    printf("Please enter your number: ");
     scanf("%d", &myNum);
-    printf("you have %d zero bits in your fucking number\n", count_zero_bits(myNum));
+    printf("\n You enetered: %d\n", myNum);
+    printf("Number of zero bits in this number including leading zeros is: %d\n", count_zero_bits(myNum));
     return 0;
 }
